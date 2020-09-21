03322191a77cab0fe98ed6df7e80935eaa995cff
Douglas Mitchell <roxonus@gmail.com>
2020-09-20 20:12:12 -0700

    created the db folder, schema.sql, seeds.sql. Also created the config folder

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+drop database if exists burgers_db
+
+create database burgers_db
+
+use burgers_db
+
+create table burgers (
+    id int not null auto_increment,
+    burger_name varchar(30)
+    devoured boolean default false,
+    primary key (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers (burger_name, devoured) values ("Bacon Cheeseburger")
+insert into burgers (burger_name, devoured) values ("Bacon Double Cheese")
+insert into burgers (burger_name, devoured) values ("Bacon Triple Cheese")
