4bacacc52edc7a2f2912c8339324ebe02c623fb3
Douglas Mitchell <roxonus@gmail.com>
2020-09-25 14:19:59 -0700

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-const express = require("express");
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// connects api routes to the server
+var routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
