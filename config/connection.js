5b1c562de7e75d53646edfa4fea833cd82b425a0
Douglas Mitchell <roxonus@gmail.com>
2020-09-30 20:02:28 -0700

    updating database for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 const mysql = require("mysql");
+var connection;
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: 'password',
-    database: 'burgers_db'
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: 'password',
+        database: 'burgers_db'
+    });
+};
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
